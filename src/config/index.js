7a844493eb47f9e210eb1488ef77dd94b685c489
chenjiajian <798095202@qq.com>
2020-07-08 20:09:12 +0800

    add: tt

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,7 @@ export const targets = [
   { id: 'weapp', name: '微信小程序' },
   { id: 'alipay', name: '支付宝小程序' },
   { id: 'swan', name: '百度小程序' },
+  { id: 'tt', name: '字节跳动小程序' },
   { id: 'qq', name: 'QQ 小程序' },
   { id: 'jd', name: '京东小程序' },
   { id: 'quickapp', name: '快应用' },
