c8fab4cc5f4953fead4a152d8e073c2b8a670e57
huangli47 <huangli@jd.com>
2021-10-19 18:59:58 +0800

    update: 更新gitee接口地址

--- src/config/index.js
+++ src/config/index.js
@@ -7,5 +7,10 @@ export const targets = [
   { id: 'jd', name: '京东小程序' },
   { id: 'quickapp', name: '快应用' },
   { id: 'rn', name: 'React Native' },
-  { id: 'h5', name: 'H5' }
+  { id: 'h5', name: 'H5' },
+  { id: 'dd', name: '钉钉小程序' },
+  { id: 'qy', name: '企业微信小程序' },
+  { id: 'feishu', name: '飞书小程序' },
+  { id: 'kwai', name: '快手小程序' },
+  { id: 'iot', name: '支付宝 IOT 小程序' }
 ]
--- src/config/repos.js
+++ src/config/repos.js
@@ -14,11 +14,17 @@ const targets = [
   { id: 'weapp', name: '微信小程序' },
   { id: 'alipay', name: '支付宝小程序' },
   { id: 'swan', name: '百度小程序' },
+  { id: 'tt', name: '字节跳动小程序' },
   { id: 'qq', name: 'QQ 小程序' },
   { id: 'jd', name: '京东小程序' },
   { id: 'quickapp', name: '快应用' },
   { id: 'rn', name: 'React Native' },
-  { id: 'h5', name: 'H5' }
+  { id: 'h5', name: 'H5' },
+  { id: 'dd', name: '钉钉小程序' },
+  { id: 'qy', name: '企业微信小程序' },
+  { id: 'feishu', name: '飞书小程序' },
+  { id: 'kwai', name: '快手小程序' },
+  { id: 'iot', name: '支付宝 IOT 小程序' }
 ]
 
 export { repos, targets }
